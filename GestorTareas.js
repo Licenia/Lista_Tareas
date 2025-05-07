80441bbf35357a7be0064ffa9aa74510888bd809
Licenia Alvear <licialvear@gmail.com>
2025-05-07 10:37:58 -0500

    feat(input): crear nueva fila al perder el foco

--- GestorTareas.js
+++ GestorTareas.js
@@ -30,7 +30,44 @@ class GestorTareas {
 
         this.tabla.appendChild(fila)
     }
+
+    agregarFilaEditable(opcionesEstado){
+        const fila = document.createElement("tr");
+
+        const celdaTarea = document.createElement("td");
+        const inputTarea = document.createElement("input");
+        inputTarea.placeholder = "Escribe una tarea";
+        celdaTarea.appendChild(inputTarea);
+        fila.appendChild(celdaTarea);
+
+        const celdaEstado = document.createElement("td");
+        const select = document.createElement("select");
+
+        opcionesEstado.forEach(opcion => {
+            const option = document.createElement("option");
+            option.value = opcion;
+            option.textContent = opcion;
+            select.appendChild(option);
+        })
+        celdaEstado.appendChild(select);
+        fila.appendChild(celdaEstado);
+
+        this.tabla.appendChild(fila);
+
+        // cuando pierde el foco, si hay texto , crea una nueva fila editable
+        inputTarea.addEventListener("blur", ()=>{
+            const valor = inputTarea.value.trim();
+            if (valor !== "") {
+                this.tabla.removeChild(fila);
+
+                this.agregarTarea(valor, opcionesEstado),
+                
+                this.agregarFilaEditable(opcionesEstado);
+            }
+        })
+    }
 }
 
 const gestor = new GestorTareas("tablaTareas");
-gestor.agregarTarea("Hacer la clase de la tabla" ,["Completada", "Pendiente"]);
+gestor.agregarFilaEditable(["Completada", "Pendiente"])
+
